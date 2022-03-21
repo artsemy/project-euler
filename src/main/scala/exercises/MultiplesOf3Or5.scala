a4835ee5a9b7286899961e7de0cc143c09b46755
artsemy <artemiy.konoplyanik@gmail.com>
2022-03-21 15:12:43 +0300

    attempt 2

--- src/main/scala/exercises/MultiplesOf3Or5.scala
+++ src/main/scala/exercises/MultiplesOf3Or5.scala
@@ -3,4 +3,7 @@ package exercises
 object MultiplesOf3Or5:
 
   def countSum(upperBorder: Int): Int =
-    (1 until upperBorder).filter(x => x % 3 == 0 || x % 5 == 0).sum
+    val d1 = (upperBorder - 1) / 3
+    val d2 = (upperBorder - 1) / 5
+    val d3 = (upperBorder - 1) / 15
+    (1 + d1) * d1 / 2 * 3 + (1 + d2) * d2 / 2 * 5 - (1 + d3) * d3 / 2 * 15
