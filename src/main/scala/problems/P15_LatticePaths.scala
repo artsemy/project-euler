e6468e2b176a5dbdf94c36ef39ccd89839628c43
artsemy <artemiy.konoplyanik@gmail.com>
2022-03-24 17:59:26 +0300

    ex15 time fix1

--- src/main/scala/problems/P15_LatticePaths.scala
+++ src/main/scala/problems/P15_LatticePaths.scala
@@ -3,9 +3,9 @@ package problems
 /*
   Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
   there are exactly 6 routes to the bottom right corner.
-  
+
   How many such routes are there through a 20×20 grid?
-  
+
   https://projecteuler.net/problem=15
 */
 
@@ -14,10 +14,8 @@ object P15_LatticePaths:
   def path(n: Int): Long =
 
     def loop(row: Int, column: Int): Long =
-      if row == n then
+      if row == n || column == n then
         1
-      else if column == n then
-        loop(row + 1, column)
       else
         loop(row, column + 1) + loop(row + 1, column)
 
