ec96b0720853c15eb77a470e75a174bd36a931f7
artsemy <artemiy.konoplyanik@gmail.com>
2022-03-29 09:58:16 +0300

    pr 16 attempt 1

---
+++ src/main/scala/problems/P16_PowerDigitSum.scala
@@ -0,0 +1,43 @@
+package problems
+
+import scala.annotation.tailrec
+
+/*
+  2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+  What is the sum of the digits of the number 2^1000?
+
+  https://projecteuler.net/problem=16
+*/
+
+object P16_PowerDigitSum:
+
+  def pDigitSum(number: Int, deg: Int): Int =
+
+    val digitsList = number.toString.map(_ - 48).reverse
+
+    @tailrec
+    def multiply(digitsList: Seq[Int], res: Seq[Int], add: Int = 0): Seq[Int] =
+      if digitsList.isEmpty then
+        if add == 0 then
+          res
+        else
+          res :+ add
+      else
+        val m = digitsList.head * number + add
+        multiply(digitsList.tail, res :+ (m % 10), m / 10)
+
+    @tailrec
+    def loop(digitsList: Seq[Int], n: Int): Int =
+      if n == 1 then
+        digitsList.sum
+      else
+        val newDigitsList = multiply(digitsList, Nil)
+        loop(newDigitsList, n - 1)
+
+    if deg == 0 then 
+      1
+    else
+      loop(digitsList, deg)
+
+  @main def run2() = println(pDigitSum(2, 1000))
---
+++ src/test/scala/problems/P16_PowerDigitSumTest.scala
@@ -0,0 +1,19 @@
+package problems
+
+import munit.FunSuite
+import problems.P16_PowerDigitSum._
+
+class P16_PowerDigitSumTest extends FunSuite:
+
+  test("power digit sum 2^0") {
+    assertEquals(pDigitSum(2, 0), 1)
+  }
+
+  test("power digit sum 2^15") {
+    assertEquals(pDigitSum(2, 15), 26)
+  }
+
+  test("power digit sum 2^1000") {
+    assertEquals(pDigitSum(2, 1000), 1366)
+  }
+
