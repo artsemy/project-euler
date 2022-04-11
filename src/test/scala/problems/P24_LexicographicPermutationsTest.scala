fcb2fe8f202dca62d2a494b97da2c310eacce099
arty <artemiy.konoplyanik@gmail.com>
2022-04-11 11:49:30 +0300

    p24 style fix 2

--- src/test/scala/problems/P24_LexicographicPermutationsTest.scala
+++ src/test/scala/problems/P24_LexicographicPermutationsTest.scala
@@ -3,20 +3,20 @@ package problems
 import munit.FunSuite
 import problems.P24_LexicographicPermutations._
 
-class P24_LexicographicPermutationsTest extends FunSuite:
+class P24_LexicographicPermutationsTest extends FunSuite :
 
   test("permutation v1 15 of 0 to 3") {
-    assertEquals(permutation(Seq(0,1,2,3), 15), Seq(2,1,0,3))
+    assertEquals(permutation(Seq(0, 1, 2, 3), 15), Seq(2, 1, 0, 3))
   }
 
   test("permutation v2 15 of 0 to 3") {
-    assertEquals(permutationV2(Seq(0,1,2,3), 15), Seq(2,1,0,3))
+    assertEquals(permutationV2(Seq(0, 1, 2, 3), 15), Seq(2, 1, 0, 3))
   }
 
   test("permutation v1 1000000 of 0 to 9") {
-    assertEquals(permutation(Seq(0,1,2,3,4,5,6,7,8,9), 1000000), Seq(2, 7, 8, 3, 9, 1, 5, 4, 6, 0))
+    assertEquals(permutation(Seq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 1000000), Seq(2, 7, 8, 3, 9, 1, 5, 4, 6, 0))
   }
 
   test("permutation v2 1000000 of 0 to 9") {
-    assertEquals(permutationV2(Seq(0,1,2,3,4,5,6,7,8,9), 1000000), Seq(2, 7, 8, 3, 9, 1, 5, 4, 6, 0))
+    assertEquals(permutationV2(Seq(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 1000000), Seq(2, 7, 8, 3, 9, 1, 5, 4, 6, 0))
   }
