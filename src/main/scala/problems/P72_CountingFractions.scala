a5f95225cdc610e39717b42a13f40ef2158271ef
artsemy <artemiy.konoplyanik@gmail.com>
2022-05-17 14:40:54 +0300

    p72 attempt 1

---
+++ src/main/scala/problems/P72_CountingFractions.scala
@@ -0,0 +1,26 @@
+package problems
+
+import utils.EulerMath.*
+import scala.annotation.tailrec
+
+/*
+  Consider the fraction, n/d, where n and d are positive integers.
+  If n<d and HCF(n,d)=1, it is called a reduced proper fraction.
+  If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+  1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+  It can be seen that there are 21 elements in this set.
+  
+  How many elements would be contained in the set of reduced proper fractions for d ≤ 1,000,000?
+  
+  https://projecteuler.net/problem=72
+*/
+
+def countingFractions(border: Int): BigInt =
+  (2 to border).map(x => fi(x, getPrimes(border).toSet)).sum
+
+def fi(n: Int, primes: Set[Int]): BigInt =
+  if primes.contains(n) then
+    n - 1
+  else
+    val seq = getPrimeDividersSeq(n).distinct
+    n * seq.map(_ - 1).product / seq.product
---
+++ src/test/scala/problems/P72_CountingFractionsTest.scala
@@ -0,0 +1,21 @@
+package problems
+
+import munit.FunSuite
+
+class P72_CountingFractionsTest extends FunSuite:
+
+  test("counting fractions 8") {
+    assertEquals(countingFractions(8), BigInt(21))
+  }
+
+  test("counting fractions 7") {
+    assertEquals(countingFractions(7), BigInt(17))
+  }
+
+  test("counting fractions 1000000") { // too slow -> 3,5 min
+    assertEquals(countingFractions(10), BigInt("303963552391"))
+  }
+
+  test("fi 8") {
+    assertEquals(fi(8, Set(2, 3, 5, 7)), BigInt(4))
+  }
